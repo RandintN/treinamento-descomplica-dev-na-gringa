55001cd1cdb6c0e19ffe502253928c1c77bcd29b
Robson Cassiano <robson.cassiano@pm.me>
2025-09-20 10:34:22 -0300

    Fix page loading

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// Remove classe que esconde o conteúdo para evitar FOUC assim que o JS carrega
+if (document && document.body) {
+  document.body.classList.remove('fouc-hidden');
+}
+
 // Lógica para o menu mobile
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const closeMobileMenuButton = document.getElementById('close-mobile-menu');
